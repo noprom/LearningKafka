1a1f2c6b6685e683e4f71c4a0fc1f31c094429a6
noprom <noprom@nopromdeMacBook-Pro.local>
2016-03-15 20:01:55 +0800

    implement device bean

---
+++ src/main/scala/cn/kingsgame/logs/bean/AccessLog.scala
@@ -0,0 +1,59 @@
+package cn.kingsgame.logs.bean
+
+import com.sun.tools.corba.se.idl.StringGen
+
+/**
+  * 金石日志访问实体
+  *
+  * Author: Noprom <tyee.noprom@qq.com>
+  * Date: 16/3/15 下午7:52.
+  */
+
+/**
+  * 客户端设别信息
+  *
+  * @param slient
+  * @param offerId
+  * @param manuFacturer
+  * @param resolution
+  * @param net
+  * @param lang
+  * @param unkownSource
+  * @param id
+  * @param androidId
+  * @param time
+  * @param mc
+  * @param mem
+  * @param sdk
+  * @param vcode
+  * @param app
+  * @param os
+  * @param apis
+  * @param sNation
+  * @param imei
+  * @param zavj8p
+  * @param cpu
+  * @param versionName
+  * @param board
+  * @param nation
+  * @param operator
+  * @param product
+  * @param deviceMD5
+  * @param producer
+  * @param brand
+  * @param imsi
+  * @param uuid
+  * @param group
+  * @param channel
+  */
+case class Device(slient: Int, offerId: String, manuFacturer: String, resolution: String,
+                  net: String, lang: String, unkownSource: String, id: String, androidId: String,
+                  time: String, mc: String, mem: String, sdk: String, vcode: String, app: String,
+                  os: String, apis: String, sNation: String, imei: String, zavj8p: String, cpu: String,
+                  versionName: String, board: String, nation: String, operator: String,
+                  product: String, deviceMD5: String, producer: String, brand: String,
+                  imsi: String, uuid: String, group: String, channel: String)
+
+class KingsGameAccessLog {
+
+}
